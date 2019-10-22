837ea2659ecc263b13feaf06908f4f94e73cf325
ajagupta <akumar298@gmail.com>
2019-10-22 04:06:53 +0100

    build package added

---
+++ card-web/build/precache-manifest.2bdcc5baaf7f3d96787fab671359489e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efbdf369969be59a000d7d316f2b773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3adf98a342c43d63b45",
+    "url": "/static/css/2.0b76d70b.chunk.css"
+  },
+  {
+    "revision": "9bb138a25c0a7ea84990",
+    "url": "/static/css/main.3ebf4d87.chunk.css"
+  },
+  {
+    "revision": "c3adf98a342c43d63b45",
+    "url": "/static/js/2.2a5070b5.chunk.js"
+  },
+  {
+    "revision": "9bb138a25c0a7ea84990",
+    "url": "/static/js/main.1cb51210.chunk.js"
+  },
+  {
+    "revision": "d8191039fe74993ea21f",
+    "url": "/static/js/runtime-main.bc12ca20.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
